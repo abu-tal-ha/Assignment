f49a3f0673552c86b45460674069812f2adeba3e
abu-tal-ha <mabutalha468@gmail.com>
2025-01-24 02:19:58 +0600

    assignment

---
+++ custom.js
@@ -0,0 +1,48 @@
+// standar way
+var firstName = "Abu";
+var age = 20;
+let lastName = "talha";
+
+console.log(firstName);
+console.log(age);
+console.log(lastName);
+
+// new practis way, first way use ;
+firstName = "Abu";
+console.log(firstName);
+
+//
+const Pi = 3.456; // allow
+// Pi = 4.456; *** not allow
+console.log(Pi);
+
+// number use
+var myName1 = "talha";
+// 1myName **** not allow
+console.log(myName1);
+
+// not standar way;
+var na$me = "abu talha";
+// -name  **** not allow
+console.log(na$me);
+
+// operetor use
+let $project = "portfolio";
+console.log($project);
+
+// Camel Case;
+const myFristName = "AbuTalha";
+console.log(myFristName);
+
+
+
+
+//  comments kora dekhbo;
+
+// Single Line Comment;
+// Another single line commnent;
+
+
+/*
+This is a Multi Line Comment Example;
+*/
