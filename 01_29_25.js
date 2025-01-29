3ae9151af9ce24c5a4b57577e276b92d441c8c21
abu-tal-ha <mabutalha468@gmail.com>
2025-01-29 16:45:23 +0600

    Arithmetic oparetors

--- 01_29_25.js
+++ 01_29_25.js
@@ -1,3 +1,5 @@
+// multi dimention array;
+
 const arrre = [
   ["talha", "faridpur", 18],
   ["sajib", "comilla", 19],
@@ -7,6 +9,21 @@ const arrre = [
 console.log(arrre);
 console.log(arrre.length);
 console.log(arrre[2][3][1]);
+
+
+// Arithmetic oparetors;
+
+let jj = 102;
+let kk = 5;
+console.log(jj + kk);
+console.log(jj - kk);
+console.log(jj / kk);
+console.log(jj ** kk);
+console.log(jj % kk);
+console.log(jj++);
+console.log(++jj);
+console.log(jj--);
+
 
 
 
