18ddbe6b9dd74a154fef9e706a913abec61f0a1c
abu-tal-ha <mabutalha468@gmail.com>
2025-01-29 16:35:45 +0600

    multi dimention arry

---
+++ 01_29_25.js
@@ -0,0 +1,13 @@
+const arrre = [
+  ["talha", "faridpur", 18],
+  ["sajib", "comilla", 19],
+  ["rezaul", "narsingi", 28, ["read", "sleep"]],
+];
+
+console.log(arrre);
+console.log(arrre.length);
+console.log(arrre[2][3][1]);
+
+
+
+
